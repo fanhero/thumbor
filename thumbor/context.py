43adc7b5f6f47cfa65bdbcfea3c7348041ee9f2d
Christian Green <christian@fanhero.com>
2017-09-01 11:11:40 -0400

    Round instead of ceil

--- thumbor/context.py
+++ thumbor/context.py
@@ -202,7 +202,7 @@ class RequestParameters:
             self.accepts_webp = 'image/webp' in request.headers.get('Accept', '')
 
     def int_or_0(self, value):
-        return 0 if value is None else math.ceil(float(value))
+        return 0 if value is None else int(float(value))
 
 
 class ContextImporter:
